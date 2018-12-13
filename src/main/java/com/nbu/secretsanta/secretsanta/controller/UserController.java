30f1877363afcf51c91a54bcfe7f914187c8ecae
DESKTOP-B1A21RN\Master <kiro972@abv.bg>
2018-12-13 11:59:23 +0200

    TestKiro

--- src/main/java/com/nbu/secretsanta/secretsanta/controller/UserController.java
+++ src/main/java/com/nbu/secretsanta/secretsanta/controller/UserController.java
@@ -3,7 +3,7 @@ package com.nbu.secretsanta.secretsanta.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+// comment
 @Controller
 public class UserController {
 
