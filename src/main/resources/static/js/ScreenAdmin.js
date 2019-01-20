7ec7e43362e282a41a2453e97d03f75340ba5bc6
Elian Kurtev <lqnko_55@outlook.com>
2019-01-20 21:26:57 +0200

    Admin page- added search input and fixed datepickers All elements are with responsive design

---
+++ src/main/resources/static/js/ScreenAdmin.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(function () {
+        $('#datetimepicker1').datetimepicker();
+        $('#datetimepicker2').datetimepicker({
+            useCurrent: false
+        });
+        $("#datetimepicker1").on("change.datetimepicker", function (e) {
+            $('#datetimepicker2').datetimepicker('minDate', e.date);
+        });
+        $("#datetimepicker2").on("change.datetimepicker", function (e) {
+            $('#datetimepicker1').datetimepicker('maxDate', e.date);
+        });
+    });
+
+});
