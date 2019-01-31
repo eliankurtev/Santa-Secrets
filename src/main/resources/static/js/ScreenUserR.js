af103b768a0bd7370b37eb142d644090e2f54b1e
Elian Kurtev <lqnko_55@outlook.com>
2019-01-31 10:26:44 +0200

    Added christmas party timer

--- src/main/resources/static/js/ScreenUserR.js
+++ src/main/resources/static/js/ScreenUserR.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 
         // Time calculations for days, hours, minutes and seconds
         var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))-2;
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
